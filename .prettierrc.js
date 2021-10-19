cc64c891e963438f6301f3f8c6662313754bce58
younoah <kr_uno_10@naver.com>
2021-10-20 00:54:33 +0900

    build: eslint, prettier 패키지 추가 및 설정

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // 코드 한줄이 maximum 80칸
+  printWidth: 80,
+  // 문자열은 홀따옴표(')로 formatting
+  singleQuote: true,
+  //코드 마지막에 세미콜론이 있게 formatting
+  semi: true,
+  //탭의 사용을 금하고 스페이스바 사용으로 대체하게 formatting
+  useTabs: false,
+  // 들여쓰기 너비는 2칸
+  tabWidth: 2,
+  // 객체나 배열을 작성 할 때, 원소 혹은 key-value의 맨 뒤에 있는 것에도 쉼표를 붙임
+  trailingComma: 'all',
+  // 대괄호 {} 사이 공백
+  bracketSpacing: true,
+  // JSX 요소 > 줄바꿈
+  jsxBracketSameLine: true,
+  // EOL 자동추가
+  endOfLine: 'auto',
+};
