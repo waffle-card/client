f184b728f9d3637ced295f6db8ce31aa94741b07
younoah <kr_uno_10@naver.com>
2021-10-21 17:22:36 +0900

    feature- 버튼 컴포넌트 개발 및 스토리북 설정 변경

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,23 @@
+const path = require('path');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+  webpackFinal: async config => {
+    config.resolve.alias['@components'] = path.resolve(
+      __dirname,
+      '../src/components',
+    );
+    config.resolve.alias['@hooks'] = path.resolve(__dirname, '../src/hooks');
+    config.resolve.alias['@contexts'] = path.resolve(
+      __dirname,
+      '../src/contexts',
+    );
+    config.resolve.alias['@pages'] = path.resolve(__dirname, '../src/pages');
+    return config;
+  },
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from '@components/base/Button';
 
 function App() {
   return (
     <div className="App">
       <h1>hello!</h1>
+      <Button>버튼</Button>
     </div>
   );
 }
