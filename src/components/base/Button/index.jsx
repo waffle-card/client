697bc02cae20cb13aad9af592147c92ee47cb317
younoah <kr_uno_10@naver.com>
2021-10-22 00:53:47 +0900

    refactor: styled 컴포넌트 방식으로 리팩토링

--- src/components/base/Button/index.jsx
+++ src/components/base/Button/index.jsx
@@ -1,11 +1,41 @@
+import styled from '@emotion/styled';
 import { useCallback } from 'react';
 import PropTypes from 'prop-types';
-import './Button.scss';
+
+const StyledButton = styled.button`
+  box-sizing: border-box;
+  display: block;
+  width: ${({ width }) => width}px;
+  height: ${({ height }) => height}px;
+  padding: 8px 6px;
+  font-size: ${({ fontsize }) => fontsize}px;
+  color: ${({ fontcolor }) => fontcolor};
+  cursor: pointer;
+  background-color: ${({ backgroundcolor }) => backgroundcolor};
+  border: none;
+  border-radius: 16px;
+  outline: none;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:disabled {
+    background-color: #888;
+  }
+
+  &:active {
+    border: solid 2px ${({ fontcolor }) => fontcolor};
+  }
+`;
 
 const Button = ({
   children,
-  size = 'small',
-  color = 'yellow',
+  width,
+  height,
+  backgroundColor,
+  fontColor,
+  fontSize,
   onClick,
   ...props
 }) => {
@@ -14,16 +44,27 @@ const Button = ({
   }, [onClick]);
 
   return (
-    <button {...props} className={`${size} ${color}`} onClick={handleClick}>
+    <StyledButton
+      {...props}
+      width={width}
+      height={height}
+      backgroundcolor={backgroundColor}
+      fontcolor={fontColor}
+      fontsize={fontSize}
+      onClick={handleClick}>
       {children}
-    </button>
+    </StyledButton>
   );
 };
 
 Button.propTypes = {
-  children: PropTypes.string.isRequired,
-  size: PropTypes.string.isRequired,
-  color: PropTypes.string.isRequired,
+  children: PropTypes.string,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+  backgroundColor: PropTypes.string.isRequired,
+  fontColor: PropTypes.string.isRequired,
+  fontSize: PropTypes.number.isRequired,
+  onClick: PropTypes.func,
 };
 
 export default Button;
--- src/stories/components/base/Button.stories.jsx
+++ src/stories/components/base/Button.stories.jsx
@@ -4,17 +4,25 @@ export default {
   title: 'Component/Base/Button',
   component: Button,
   argTypes: {
-    size: {
-      name: 'size',
-      defaultValue: 'small',
-      options: ['small', 'large'],
-      control: { type: 'inline-radio' },
+    width: {
+      defaultValue: 550,
+      control: { type: 'number' },
     },
-    color: {
-      name: 'color',
-      defaultValue: 'yellow',
-      options: ['yellow', 'white'],
-      control: { type: 'inline-radio' },
+    height: {
+      defaultValue: 56,
+      control: { type: 'number' },
+    },
+    fontColor: {
+      defaultValue: 'white',
+      control: { type: 'color' },
+    },
+    backgroundColor: {
+      defaultValue: '#FFD039',
+      control: { type: 'color' },
+    },
+    fontSize: {
+      defaultValue: 18,
+      control: { type: 'number' },
     },
     onClick: { action: 'onClick' },
   },
@@ -26,6 +34,30 @@ export const Default = args => {
       <Button {...args}>가입하기</Button>
       <br />
       <Button {...args}>입장하기</Button>
+    </>
+  );
+};
+
+export const Test = () => {
+  const buttonOptions = {
+    fontColor: 'white',
+    backgroundColor: '#FFD039',
+    width: 300,
+    height: 32,
+    fontSize: 12,
+  };
+  return (
+    <>
+      <Button {...buttonOptions}>구조분해 버튼</Button>
+      <br />
+      <Button
+        width={400}
+        height={32}
+        fontColor={'#FFD039'}
+        backgroundColor={'royalblue'}
+        fontSize={18}>
+        속성지정 버튼
+      </Button>
     </>
   );
 };
