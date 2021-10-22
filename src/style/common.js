e0382e4a5458a2f820f43733b9eb60f7f4c6952f
yoon <yunyun40550082@gmail.com>
2021-10-22 22:58:10 +0900

    style: 기존 fontSize객체를 fontStyle로이름  변경 & fontSize객체 새로 생성

--- src/style/common.js
+++ src/style/common.js
@@ -35,6 +35,14 @@ export const Common = {
   },
 
   fontSize: {
+    micro: `0.75rem`,
+    small: `0.875rem`,
+    base: `1rem`,
+    medium: `1.125rem`,
+    large: `1.5rem`,
+  },
+
+  fontStyle: {
     micro: () => `
       font-size: 0.75rem;
       line-height: 16px;
