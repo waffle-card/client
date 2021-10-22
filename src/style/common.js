959a7848611b28ab31875a5951caea14c1712f35
yoon <yunyun40550082@gmail.com>
2021-10-22 19:19:15 +0900

    style: 공통 스타일 값 변수 지정

---
+++ src/style/common.js
@@ -0,0 +1,52 @@
+export const Common = {
+  colors: {
+    point: 'rgba(255, 208, 57, 1)',
+
+    blue: 'rgba(81, 223, 253, 1)',
+    green: 'rgba(57, 219, 178, 1)',
+    pink: 'rgba(219, 69, 168, 1)',
+    yellow: 'rgba(247, 173, 42, 1)',
+    purple: 'rgba(162, 75, 240, 1)',
+    red: 'rgba(233, 53, 80, 1)',
+    indigo: 'rgba(92, 107, 192, 1)',
+    teal: 'rgba(38, 166, 154, 1)',
+    lime: 'rgba(212, 225, 87, 1)',
+    brown: 'rgba(141, 110, 99, 1)',
+    deepOrange: 'rgba(255, 112, 67, 1)',
+    blueGrey: 'rgba(120, 144, 156, 1)',
+
+    background: 'rgba(26, 31, 39, 1)',
+    background_menu: 'rgba(255, 255, 255, 0.1)',
+    background_pointer: 'rgba(255, 255, 255, 0.2)',
+    background_modal: 'rgba(43, 51, 63, 1)',
+    border: 'rgba(255, 255, 255, 0.5)',
+
+    primary: 'rgba(255, 255, 255, 1)',
+    secondary: 'rgba(255, 255, 255, 0.6)',
+    tertiary: 'rgba(255, 255, 255, 0.35)',
+  },
+
+  shadow: {
+    menu: '3px 2px 8px rgba(0, 0, 0, 0.7)',
+    modal: '7px 14px 20px rgba(0, 0, 0, 1.2)',
+    card: '2px 5px 6px rgba(0, 0, 0, 1.2)',
+    chattingHeader: '2px 5px 6px rgba(0, 0, 0, 1.2)',
+    chattingElement: '2px 5px 6px rgba(0, 0, 0, 1.2)',
+  },
+
+  fontWeight: {
+    regular: 400,
+    medium: 500,
+    bold: 700,
+  },
+
+  spacing: {
+    base: 8,
+  },
+
+  media: {
+    sm: '(max-width: 768px)',
+    md: '(min-width : 769px) and (max-width : 1200px)',
+    lg: '(min-width: 1201px)',
+  },
+};
