519a44ebc800ec91c7d0e47ff363643f83c5817d
younoah <kr_uno_10@naver.com>
2021-10-26 01:00:38 +0900

    feat: base Component - EmojiPicker 구현

---
+++ src/components/base/EmojiPicker/index.jsx
@@ -0,0 +1,69 @@
+import styled from '@emotion/styled';
+import React, { useCallback, useRef, useState } from 'react';
+import { Button } from '@components';
+import Common from '@styles';
+import Picker from 'emoji-picker-react';
+import PropTypes from 'prop-types';
+
+const Container = styled.div`
+  display: inline-block;
+  position: relative;
+  margin: 0;
+`;
+
+const PickerWrapper = styled.div`
+  position: absolute;
+  top: 48px;
+  left: 0;
+  z-index: 2;
+  animation: fadeIn 0.5s ease-in forwards;
+`;
+
+const EmojiInput = styled.input`
+  display: none;
+`;
+
+const EmojiPicker = ({ disabled, onChange, ...props }) => {
+  const [showEmojiPicker, togglePicker] = useState(() => false);
+  const ref = useRef();
+  const toggleEmojiPicker = () => togglePicker(prev => !prev);
+
+  const handleEmojiClick = useCallback(
+    (_, emojiObject) => {
+      ref.current.value = emojiObject.emoji;
+      onChange && onChange(emojiObject.emoji);
+      toggleEmojiPicker();
+    },
+    [onChange],
+  );
+
+  return (
+    <Container {...props}>
+      <Button
+        width={88}
+        height={40}
+        fontSize={Common.fontSize.small}
+        disabled={disabled}
+        onClick={toggleEmojiPicker}>
+        선택
+      </Button>
+      <EmojiInput type="text" name="emoji" ref={ref} />
+      {showEmojiPicker && (
+        <PickerWrapper>
+          <Picker onEmojiClick={handleEmojiClick} />
+        </PickerWrapper>
+      )}
+    </Container>
+  );
+};
+
+EmojiPicker.protoTypes = {
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func,
+};
+
+EmojiPicker.defaultProps = {
+  disabled: false,
+};
+
+export default EmojiPicker;
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export { default as Button } from './base/Button';
+export { default as EmojiPicker } from './base/EmojiPicker';
