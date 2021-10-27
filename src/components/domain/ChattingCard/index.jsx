664bef329547b98b8284d2c91366f923a38205ba
naeunchan <eunchan0492@naver.com>
2021-10-27 23:20:03 +0900

    feat: 좋아요, 즐겨찾기 toggle count 구현

--- src/components/domain/ChattingCard/index.jsx
+++ src/components/domain/ChattingCard/index.jsx
@@ -3,9 +3,11 @@ import Modal from '@components/base/Modal';
 import Icons from '@components/base/Icons';
 import Text from '@components/base/Text';
 import Common from '@styles';
+import { useEffect, useState } from 'react';
 
 const Header = styled.div`
-  height: 200px;
+  min-height: 200px;
+  max-height: 250px;
   padding: 15px 40px 0 40px;
   background-color: royalblue;
   border-top-left-radius: 16px;
@@ -49,16 +51,17 @@ const SecondHashtags = styled.div`
 const Body = styled.div`
   display: flex;
   flex-direction: column;
-  height: 570px;
+  min-height: 98px;
+  max-height: 400px;
   padding: 10px;
   background-color: transparent;
-  border-bottom-left-radius: 16px;
-  border-bottom-right-radius: 16px;
   background-color: ${Common.colors.background_modal};
   overflow-y: auto;
 `;
 
 const Image = styled.img`
+  max-width: 52px;
+  max-height: 52px;
   width: 52px;
   height: 52px;
   src: ${({ src }) => src};
@@ -84,6 +87,29 @@ const ChatBox = styled.div`
   padding-right: 20px;
 `;
 
+const Footer = styled.div`
+  display: flex;
+  background-color: ${Common.colors.background_modal};
+  min-height: 180px;
+  max-height: 200px;
+  border-bottom-left-radius: 16px;
+  border-bottom-right-radius: 16px;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+`;
+
+const InputBox = styled.div`
+  display: flex;
+  background-color: ${Common.colors.speech_bubble};
+  width: 100%;
+  height: 144px;
+  min-height: 44px;
+  max-height: 144px;
+  border-radius: 16px;
+  padding: 10px 20px 10px 20px;
+`;
+
 const ChattingCard = ({
   children,
   width,
@@ -92,6 +118,35 @@ const ChattingCard = ({
   informations,
   ...props
 }) => {
+  // API가 필요한 부분
+  const myId = 1;
+  const title = '내이름은 제페토';
+
+  const [likeCount, setLikeCount] = useState(0);
+  const [favoriteCount, setFavoriteCount] = useState(0);
+  const [like, setLike] = useState(false);
+  const [favorite, setFavorite] = useState(false);
+
+  const handleLikeClick = () => {
+    if (like) {
+      setLikeCount(likeCount - 1);
+      setLike(false);
+    } else {
+      setLikeCount(likeCount + 1);
+      setLike(true);
+    }
+  };
+
+  const handleFavoriteClick = () => {
+    if (favorite) {
+      setFavoriteCount(favoriteCount - 1);
+      setFavorite(false);
+    } else {
+      setFavoriteCount(favoriteCount + 1);
+      setFavorite(true);
+    }
+  };
+
   const getChatLog = () => [
     {
       id: 1,
@@ -125,8 +180,6 @@ const ChattingCard = ({
     },
   ];
 
-  const myId = 1;
-
   const showChat = logs => {
     return logs.map((log, index) => (
       <ChatContainer isMine={log.id === myId} key={index}>
@@ -139,50 +192,71 @@ const ChattingCard = ({
     ));
   };
 
+  const firstHashtags = [
+    '#안녕하세요반갑습니다',
+    '#10글자로만들어봐요',
+    '우리모두함께놀아봐요',
+  ];
+  const secondHashtags = ['#해쉬태그를만들어봐요', '#최대한길게길게갑시다'];
+
+  const hashtagsDiv = hashtags => hashtags.map(hashtag => <div>{hashtag}</div>);
+
+  //
+
   return (
-    <Modal width={width} height={height} style={{ padding: 0 }} visible>
-      <Header backgroundColor={backgroundColor}>
+    <Modal
+      width={width}
+      height={height}
+      style={{ padding: 0, display: 'flex', flexDirection: 'column' }}
+      visible>
+      <Header height={height} backgroundColor={backgroundColor}>
         <Top>
           <Icons style={{ width: '120px' }}>
             <Icons.ArrowBack fontSize={32}></Icons.ArrowBack>
           </Icons>
           <Title style={{ width: '120px' }}>
             <Image src={'https://picsum.photos/200'} />
-            <Text>내이름은 제페토</Text>
+            <Text>{title}</Text>
           </Title>
           <Icons style={{ width: '120px' }}>
             <Icons.Like
+              active={like}
+              onClick={handleLikeClick}
               style={{
                 fontSize: '32px',
                 color: 'white',
                 marginRight: '5px',
               }}></Icons.Like>
-            <Text block style={{ color: 'white', marginRight: '5px' }}>
-              152
+            <Text
+              block
+              style={{ color: 'white', marginRight: '5px', paddingTop: '3px' }}>
+              {likeCount}
             </Text>
             <Icons.Favorite
+              active={favorite}
+              onClick={handleFavoriteClick}
               style={{
                 fontSize: '32px',
                 color: 'white',
                 marginRight: '5px',
               }}></Icons.Favorite>
-            <Text block style={{ color: 'white', marginRight: '5px' }}>
-              51
+            <Text
+              block
+              style={{ color: 'white', marginRight: '5px', paddingTop: '3px' }}>
+              {favoriteCount}
             </Text>
           </Icons>
         </Top>
         <Hr />
-        <FirstHashtags>
-          <div>#안녕하세요반갑습니다</div>
-          <div>#10글자로만들어봐요</div>
-          <div>#우리모두함께놀아봐요</div>
-        </FirstHashtags>
-        <SecondHashtags>
-          <div>#해쉬태그를만들어봐요</div>
-          <div>#최대한길게길게갑시다</div>
-        </SecondHashtags>
+        <FirstHashtags>{hashtagsDiv(firstHashtags)}</FirstHashtags>
+        <SecondHashtags>{hashtagsDiv(secondHashtags)}</SecondHashtags>
       </Header>
       <Body>{showChat(getChatLog())}</Body>
+      <Footer>
+        <InputBox>
+          <Text>메세지를 입력하세요</Text>
+        </InputBox>
+      </Footer>
     </Modal>
   );
 };
