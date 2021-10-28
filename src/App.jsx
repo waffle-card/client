63ab88b6ef7a294c3ef8637105311c6a9fda8f6a
yoon <yunyun40550082@gmail.com>
2021-10-28 18:35:26 +0900

    feat: 라우터 구현 시도1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { Route } from 'react-router-dom';
+import Home from '@pages/Home';
+import NotFoundPage from '@pages/NotFoundPage';
+import { CardEditForm } from '@components';
+import ChattingCard from '@components/domain/ChattingCard';
+
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Route path="/" exact={true} component={Home} />
+
+      {/* <Route path="/login" component={} />
+      <Route path="/signup" component={} />
+      <Route path="/my-page" component={} /> */}
+
+      <Route path="/cards/today" exact={true} component={Home} />
+      <Route path="/cards/my" exact={true} component={Home} />
+      <Route path="/cards/favorite" exact={true} component={Home} />
+
+      <Route path="/card/create" exact={true} component={CardEditForm} />
+      <Route path="/card/chat:cardId" exact={true} component={ChattingCard} />
+      <Route path="/card/update:cardId" exact={true} component={CardEditForm} />
+
+      <Route path="*" component={NotFoundPage} />
+    </div>
+  );
 }
 
 export default App;
