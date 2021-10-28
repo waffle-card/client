36a90d9dbdaf8f8cedfaed08e06d37e2df5729e3
Jieun Kim <jieun.kim0413@gmail.com>
2021-10-28 18:18:49 +0900

    feat: #65/Page - registerPage 구현 (#66)
    * git
    * Add Register page

---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,12 @@
+import Common from '@styles';
+import Text from '@components/text';
+
+const Register = () => {
+  return (
+    <>
+      <Text color={Common.colors.point}>회원가입</Text>
+    </>
+  );
+};
+
+export default Register;
