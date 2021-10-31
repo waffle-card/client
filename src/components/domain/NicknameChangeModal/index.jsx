eb8dd1f66af5ef2f146202bb88b868ef15d93534
yoon <yunyun40550082@gmail.com>
2021-11-01 04:40:17 +0900

    feat: 닉네임 변경 모달 컴포넌트 생성 & 셋팅

---
+++ src/components/domain/NicknameChangeModal/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Common from '@styles';
+import styled from '@emotion/styled';
+import { useForm } from '@hooks';
+import { Modal, Text, Button, Input, BackButton, Spinner } from '@components';
+
+const NicknameChangeModal = ({ ...props }) => {
+  return <Modal visible> </Modal>;
+};
+
+export default NicknameChangeModal;
---
+++ src/stories/components/domain/NicknameChangeModal.stories.jsx
@@ -0,0 +1,10 @@
+import { NicknameChangeModal } from '@components';
+
+export default {
+  title: 'Component/Domain/NicknameChangeModal',
+  component: NicknameChangeModal,
+};
+
+export const Default = args => {
+  return <NicknameChangeModal />;
+};
