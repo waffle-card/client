5881d0b80e628531bcb82811b2e97ecd4298a894
yoon <yunyun40550082@gmail.com>
2021-10-31 21:23:49 +0900

    refactor: home컴포넌트 HomePage로 이름 변경 & 스토리북 폴더 Page로 변경

---
+++ src/stories/pages/HomePage.stories.jsx
@@ -0,0 +1,10 @@
+import HomePage from '@pages/HomePage';
+
+export default {
+  title: 'page/HomePage',
+  component: HomePage,
+};
+
+export const Default = () => {
+  return <HomePage></HomePage>;
+};
