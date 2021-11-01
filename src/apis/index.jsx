7ce4a2732bdb5cd1b0995aabe5ef9fb3b5362795
younoah <kr_uno_10@naver.com>
2021-11-01 20:22:07 +0900

    refactor: api 리팩토링 및 TODO 체크

--- src/apis/index.jsx
+++ src/apis/index.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useSessionStorage } from '@hooks';
 
 const API_END_POINT = 'http://13.209.30.200';
-const TEAM_ID = '616a204c22996f0bc94f6e17';
+const CHANNEL_ID = '616a204c22996f0bc94f6e17';
 
 const setInterceptors = (instance, auth) => {
   auth &&
@@ -32,41 +32,49 @@ const createInstance = options => {
   const instance = axios.create({ baseURL: API_END_POINT, ...options });
   return setInterceptors(instance);
 };
-const instance = createInstance();
+const request = createInstance();
 
 const createInstanceWithAuth = options => {
   const instance = axios.create({ baseURL: API_END_POINT, ...options });
   return setInterceptors(instance, true);
 };
-const auth = createInstanceWithAuth();
+const authRequest = createInstanceWithAuth();
 
 const authApi = {
-  getAuthUser: () => auth.get('/auth-user'),
-  signUp: userInfo => instance.post('/signup', userInfo),
-  login: userInfo => instance.post('/login', userInfo),
-  logout: () => instance.post('/logout'),
+  getAuthUser: () => authRequest.get('/auth-user'),
+  signUp: userInfo => request.post('/signup', userInfo),
+  login: userInfo => request.post('/login', userInfo),
+  logout: () => request.post('/logout'),
 };
 
 const userApi = {
-  getUserInfo: userId => instance.get(`users/${userId}`),
-  putUserName: userName => auth.put('settings/update-user', userName),
-  putUserPassword: userPassword =>
-    auth.put('settings/update-password', userPassword),
+  getUserInfo: userId => request.get(`users/${userId}`),
+  putUserName: userName =>
+    authRequest.put('settings/update-user', { username: userName }),
+  putUserPassword: password =>
+    authRequest.put('settings/update-password', { password }),
 };
 
 const postApi = {
   getUserPostList: (userId, params) =>
-    instance.get(`posts/author/${userId}`, { params }),
-  getChannelPostList: (channelId, params) =>
-    instance.get(`posts/channel/${channelId}`, { params }),
-  createPost: post => auth.post('posts/create', post),
-  readPost: postId => instance.post(`posts/${postId}`),
-  updatePost: post => auth.put('posts/update', post),
-  deletePost: postId => auth.delete('posts/delete', postId),
-  createPostLike: postId => auth.post('likes/create', postId),
-  deletePostLike: postId => auth.delete('likes/delete', postId),
-  createPostComment: commentInfo => auth.post('comments/create', commentInfo),
-  deletePostComment: commentId => auth.delete('comments/delete', commentId),
+    request.get(`posts/author/${userId}`, { params }),
+  getUserFavoritePostList: userId => {}, // TODO:  API 확인 후 리팩토링
+  getChannelPostList: params =>
+    request.get(`posts/channel/${CHANNEL_ID}`, { params }),
+  createPost: post =>
+    authRequest.post('posts/create', { ...post, channelId: CHANNEL_ID }),
+  getPost: postId => request.post(`posts/${postId}`),
+  updatePost: post => authRequest.put('posts/update', post),
+  deletePost: postId => authRequest.delete('posts/delete', postId),
+  createPostLike: postId => authRequest.post('likes/create', postId),
+  deletePostLike: postId => authRequest.delete('likes/delete', postId),
+  createPostFavorite: postId => {}, // TODO:  API 확인 후 리팩토링
+  deletePostFavorite: postId => {}, // TODO:  API 확인 후 리팩토링
+  getPostComment: commentId => {}, // TODO:  API 확인 후 리팩토링
+  createPostComment: commentInfo =>
+    authRequest.post('comments/create', commentInfo),
+  deletePostComment: commentId =>
+    authRequest.delete('comments/delete', commentId),
 };
 
-export { authApi, userApi, postApi };
+export { authApi, userApi, postApi, request, authRequest };
