a2cb52476ae23ad63f47de405370c5bc76f518fd
younoah <kr_uno_10@naver.com>
2021-11-04 01:19:39 +0900

    feat: 즐겨찾기, 좋아요 api 구현

--- src/apis/index.jsx
+++ src/apis/index.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const API_END_POINT = 'http://13.209.30.200';
 const CHANNEL_ID = '616a204c22996f0bc94f6e17';
+// const CHANNEL_ID = '6182ac3fe1ecd063dabf10d0'; // new
 
 const setInterceptors = (instance, auth) => {
   auth &&
@@ -97,67 +98,186 @@ const cardApi = {
     authRequest.delete('posts/delete', { data: { id: cardId } }),
   // createCardLike: postId => authRequest.post('likes/create', postId),
   // deleteCardLike: postId => authRequest.delete('likes/delete', postId),
-  addLikeCard: async cardId => {
-    const response = await cardApi.getCard(cardId);
-    const {
-      title: emoji,
-      cardColor,
-      hashTags,
-      likeUsers,
-      bookmarkUsers,
-    } = response.data;
-    const cardFormData = new FormData();
-    cardFormData.append('postId', cardId);
-    cardFormData.append('title', emoji);
-    cardFormData.append('image', null);
-    cardFormData.append('channelId', CHANNEL_ID);
-    cardFormData.append(
-      'meta',
-      JSON.stringify({
+  addLikeCard: async (userId, cardId) => {
+    try {
+      const userInfoResponse = await userApi.getUserInfo(userId);
+      const { likeCardList = [], bookmarkCardList = [] } = JSON.parse(
+        userInfoResponse.data.meta,
+      );
+      if (!likeCardList.includes(cardId)) {
+        likeCardList.push(cardId);
+      }
+
+      const userInfo = {
+        fullName: userInfoResponse.data.fullName,
+        username: userInfoResponse.data.fullName,
+        meta: JSON.stringify({ likeCardList, bookmarkCardList }),
+      };
+      console.log(userInfo);
+      authRequest.put('settings/update-user', userInfo);
+
+      const cardInfoResponse = await cardApi.getCard(cardId);
+      const {
+        title: emoji,
         cardColor,
-        hashTags,
-        likeUsers: likeUsers,
-        bookmarkUsers: bookmarkUsers,
-      }),
-    );
-    authRequest.put('posts/update', cardFormData);
-  },
-  deleteLikeCard: postId => authRequest.delete('likes/delete', postId),
-  addBookMarkCard: async (userId, cardId) => {
-    const response = await userApi.getUserInfo(userId);
-    const { likeCardList = [], bookmarkCardList = [] } = JSON.parse(
-      response.data.meta,
-    );
-    if (!bookmarkCardList.includes(cardId)) {
-      bookmarkCardList.push(cardId);
+        hashTags = [],
+        likeUsers = [],
+        bookmarkUsers = [],
+      } = cardInfoResponse.data;
+      const cardFormData = new FormData();
+      cardFormData.append('postId', cardId);
+      cardFormData.append('title', emoji);
+      cardFormData.append('image', null);
+      cardFormData.append('channelId', CHANNEL_ID);
+      cardFormData.append(
+        'meta',
+        JSON.stringify({
+          cardColor,
+          hashTags,
+          likeUsers: likeUsers.push(userId),
+          bookmarkUsers: bookmarkUsers,
+        }),
+      );
+      authRequest.put('posts/update', cardFormData);
+    } catch (e) {
+      console.log(e);
     }
-
-    const userInfo = {
-      fullName: response.data.fullName,
-      username: response.data.fullName,
-      meta: JSON.stringify({ likeCardList, bookmarkCardList }),
-    };
-    console.log(userInfo);
-    authRequest.put('settings/update-user', userInfo);
   },
-  deleteBookMarkCard: async (userId, cardId) => {
-    const response = await userApi.getUserInfo(userId);
-    const { likeCardList = [], bookmarkCardList = [] } = JSON.parse(
-      response.data.meta,
-    );
-    if (bookmarkCardList.includes(cardId)) {
-      bookmarkCardList.splice(bookmarkCardList.indexOf(cardId), 1);
+  deleteLikeCard: async (userId, cardId) => {
+    try {
+      const response = await userApi.getUserInfo(userId);
+      const { likeCardList = [], bookmarkCardList = [] } = JSON.parse(
+        response.data.meta,
+      );
+      if (likeCardList.includes(cardId)) {
+        likeCardList.splice(likeCardList.indexOf(cardId), 1);
+      }
+
+      const userInfo = {
+        fullName: response.data.fullName,
+        username: response.data.fullName,
+        meta: JSON.stringify({ likeCardList, bookmarkCardList }),
+      };
+      console.log(userInfo);
+      authRequest.put('settings/update-user', userInfo);
+
+      const cardInfoResponse = await cardApi.getCard(cardId);
+      const {
+        title: emoji,
+        cardColor,
+        hashTags = [],
+        likeUsers = [],
+        bookmarkUsers = [],
+      } = cardInfoResponse.data;
+      const cardFormData = new FormData();
+      cardFormData.append('postId', cardId);
+      cardFormData.append('title', emoji);
+      cardFormData.append('image', null);
+      cardFormData.append('channelId', CHANNEL_ID);
+      cardFormData.append(
+        'meta',
+        JSON.stringify({
+          cardColor,
+          hashTags,
+          likeUsers: likeUsers.filter(id => id !== userId),
+          bookmarkUsers: bookmarkUsers,
+        }),
+      );
+      authRequest.put('posts/update', cardFormData);
+    } catch (error) {
+      console.log(error);
     }
-
-    const userInfo = {
-      fullName: response.data.fullName,
-      username: response.data.fullName,
-      meta: JSON.stringify({ likeCardList, bookmarkCardList }),
-    };
-    console.log(userInfo);
-    authRequest.put('settings/update-user', userInfo);
   },
-  getCardComment: commentId => {}, // TODO:  API 확인 후 리팩토링
+  addBookmarkCard: async (userId, cardId) => {
+    try {
+      const userInfoResponse = await userApi.getUserInfo(userId);
+      const { likeCardList = [], bookmarkCardList = [] } = JSON.parse(
+        userInfoResponse.data.meta,
+      );
+      if (!bookmarkCardList.includes(cardId)) {
+        bookmarkCardList.push(cardId);
+      }
+
+      const userInfo = {
+        fullName: userInfoResponse.data.fullName,
+        username: userInfoResponse.data.fullName,
+        meta: JSON.stringify({ likeCardList, bookmarkCardList }),
+      };
+      console.log(userInfo);
+      authRequest.put('settings/update-user', userInfo);
+
+      const cardInfoResponse = await cardApi.getCard(cardId);
+      const {
+        title: emoji,
+        cardColor,
+        hashTags = [],
+        likeUsers = [],
+        bookmarkUsers = [],
+      } = cardInfoResponse.data;
+      const cardFormData = new FormData();
+      cardFormData.append('postId', cardId);
+      cardFormData.append('title', emoji);
+      cardFormData.append('image', null);
+      cardFormData.append('channelId', CHANNEL_ID);
+      cardFormData.append(
+        'meta',
+        JSON.stringify({
+          cardColor,
+          hashTags,
+          likeUsers: likeUsers,
+          bookmarkUsers: bookmarkUsers.push(userId),
+        }),
+      );
+      authRequest.put('posts/update', cardFormData);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  deleteBookmarkCard: async (userId, cardId) => {
+    try {
+      const response = await userApi.getUserInfo(userId);
+      const { likeCardList = [], bookmarkCardList = [] } = JSON.parse(
+        response.data.meta,
+      );
+      if (bookmarkCardList.includes(cardId)) {
+        bookmarkCardList.splice(bookmarkCardList.indexOf(cardId), 1);
+      }
+
+      const userInfo = {
+        fullName: response.data.fullName,
+        username: response.data.fullName,
+        meta: JSON.stringify({ likeCardList, bookmarkCardList }),
+      };
+      console.log(userInfo);
+      authRequest.put('settings/update-user', userInfo);
+
+      const cardInfoResponse = await cardApi.getCard(cardId);
+      const {
+        title: emoji,
+        cardColor,
+        hashTags = [],
+        likeUsers = [],
+        bookmarkUsers = [],
+      } = cardInfoResponse.data;
+      const cardFormData = new FormData();
+      cardFormData.append('postId', cardId);
+      cardFormData.append('title', emoji);
+      cardFormData.append('image', null);
+      cardFormData.append('channelId', CHANNEL_ID);
+      cardFormData.append(
+        'meta',
+        JSON.stringify({
+          cardColor,
+          hashTags,
+          likeUsers: likeUsers,
+          bookmarkUsers: bookmarkUsers.filter(id => id !== userId),
+        }),
+      );
+      authRequest.put('posts/update', cardFormData);
+    } catch (error) {
+      console.log(error);
+    }
+  },
   createCardComment: commentInfo =>
     authRequest.post('comments/create', commentInfo),
   deleteCardComment: commentId =>
