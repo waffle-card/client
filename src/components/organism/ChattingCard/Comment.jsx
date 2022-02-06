c9ebc26c9ef00ea74e6a0061c0eb5051a28f8d93
younoah <kr_uno_10@naver.com>
2022-02-06 13:42:21 +0900

    feat: comment 레이아웃

--- src/components/organism/ChattingCard/Comment.jsx
+++ src/components/organism/ChattingCard/Comment.jsx
@@ -1,10 +1,43 @@
+import React, { useRef } from 'react';
 import styled from '@emotion/styled';
-import React from 'react';
+import Common from '@styles';
+import { EditBox } from '@components';
 
-const Comment = ({ ...props }) => {
-  return <TextArea></TextArea>;
+const Comment = ({ type = 'basic', commentData, ...props }) => {
+  const ref = useRef(null);
+  // const scrollHeight = ref.current.scrollHeight;
+  console.log(ref);
+
+  return (
+    <Container>
+      <EditBox />
+      <TextArea
+        ref={ref}
+        readOnly
+        value={`${commentData.userName} : ${commentData.text}`}
+      />
+    </Container>
+  );
 };
 
-const TextArea = styled.textarea``;
+const Container = styled.div`
+  display: inline-flex;
+  /* justify-content: center; */
+  /* align-items: center; */
+  background-color: ${Common.colors.speech_bubble};
+  border-radius: 1rem;
+`;
+
+const TextArea = styled.textarea`
+  /* width: 100%; */
+  margin: 1rem;
+  border: none;
+  border-radius: 8px 0 0 8px;
+  outline: none;
+  background-color: transparent;
+  resize: none;
+  font-size: ${Common.fontSize.medium};
+  color: ${Common.colors.primary};
+`;
 
 export default Comment;
--- src/components/organism/ChattingCard/CommentList.jsx
+++ src/components/organism/ChattingCard/CommentList.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Comment } from './Comment';
+import Comment from './Comment';
 
 const CommentList = () => {
-  return <Container>{/* <Comment /> */}</Container>;
+  return (
+    <Container>
+      <Comment userName={'유저'} text={'안녕하세요!'} />
+    </Container>
+  );
 };
 
 const Container = styled.div``;
