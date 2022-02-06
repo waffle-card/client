35476ac351afe59549a8a36f4065e3a5191b3732
younoah <kr_uno_10@naver.com>
2022-02-06 23:35:02 +0900

    feat: 유저정보 수정 api 모듈 추가

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -4,6 +4,8 @@ const userApi = {
   signup: userInfo => request.post('/auth/signup', userInfo),
   login: userInfo => request.post('/auth/login', userInfo),
   me: () => authRequest.get(`/auth/me`),
+  updateUser: ({ name, password }) =>
+    authRequest.put('/auth/update', { name, password }),
 };
 
 export default userApi;
