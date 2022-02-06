9f618951103c9dfc56eeee1175218ae27af52ca4
younoah <kr_uno_10@naver.com>
2022-02-06 18:31:17 +0900

    refactor: Comment 컴포넌트 구현

--- src/components/organism/ChattingCard/Comment.jsx
+++ src/components/organism/ChattingCard/Comment.jsx
@@ -1,19 +1,60 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from '@emotion/styled';
 import Common from '@styles';
 import { EditBox } from '@components';
+import TextareaAutosize from '@mui/base/TextareaAutosize';
+import { useHover } from '@hooks';
 
-const Comment = ({ type = 'basic', commentData, ...props }) => {
-  const ref = useRef(null);
-  // const scrollHeight = ref.current.scrollHeight;
-  console.log(ref);
+const getLongestLineLength = paragraph => {
+  let longestLineLength = 0;
+  const lines = paragraph.split('\n');
+
+  lines.forEach(line => {
+    if (line.length > longestLineLength) {
+      longestLineLength = line.length;
+    }
+  });
+
+  return longestLineLength;
+};
+
+const Comment = ({
+  type = 'basic',
+  onClickUpdate,
+  onClickDelete,
+  commentData,
+  ...props
+}) => {
+  const [containerRef, hover] = useHover();
+  const textAreaRef = useRef();
+
+  const handleClickUpdate = e => {
+    if (type !== 'edit') return;
+    onClickUpdate && onClickUpdate(e);
+  };
+
+  const handleClickDelete = e => {
+    if (type !== 'edit') return;
+    onClickDelete && onClickDelete(e);
+  };
+
+  useEffect(() => {
+    const width = getLongestLineLength(textAreaRef.current.value);
+    containerRef.current.style.width = width * 16 + 'px';
+  }, [containerRef, textAreaRef]);
 
   return (
-    <Container>
-      <EditBox />
+    <Container ref={containerRef} {...props}>
+      {type === 'edit' && hover && (
+        <StyledEditBox
+          onClickUpdate={handleClickUpdate}
+          onClickDelete={handleClickDelete}
+        />
+      )}
       <TextArea
-        ref={ref}
+        ref={textAreaRef}
         readOnly
+        col={'400px'}
         value={`${commentData.userName} : ${commentData.text}`}
       />
     </Container>
@@ -21,23 +62,38 @@ const Comment = ({ type = 'basic', commentData, ...props }) => {
 };
 
 const Container = styled.div`
-  display: inline-flex;
-  /* justify-content: center; */
-  /* align-items: center; */
-  background-color: ${Common.colors.speech_bubble};
-  border-radius: 1rem;
+  display: inline-block;
+  position: relative;
+  max-width: 70%;
+  min-width: 30%;
+  margin: 1rem;
 `;
 
-const TextArea = styled.textarea`
-  /* width: 100%; */
-  margin: 1rem;
-  border: none;
-  border-radius: 8px 0 0 8px;
-  outline: none;
-  background-color: transparent;
-  resize: none;
-  font-size: ${Common.fontSize.medium};
+const TextArea = styled(TextareaAutosize)`
+  width: 100%;
+  padding: 1rem;
+  border-radius: 1rem;
+  background-color: ${Common.colors.speech_bubble};
   color: ${Common.colors.primary};
+  outline: none;
+  border: none;
+  resize: none;
+
+  @media ${Common.media.sm} {
+    font-size: ${Common.fontSize.small};
+  }
+  @media ${Common.media.md} {
+    font-size: ${Common.fontSize.base};
+  }
+  @media ${Common.media.lg} {
+    font-size: ${Common.fontSize.medium};
+  }
+`;
+
+const StyledEditBox = styled(EditBox)`
+  position: absolute;
+  top: -10px;
+  right: 0px;
 `;
 
 export default Comment;
--- src/components/organism/ChattingCard/CommentList.jsx
+++ src/components/organism/ChattingCard/CommentList.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import styled from '@emotion/styled';
 import Comment from './Comment';
 
+const text = `안녕하세요.
+제 이름은 정윤호 입니다.제 이름제 이름은 정윤호 입니다.제 이름은 정윤호 입니다.
+제 이름은 정윤호 입니다.`;
+
 const CommentList = () => {
   return (
     <Container>
-      <Comment userName={'유저'} text={'안녕하세요!'} />
+      <Comment
+        type="edit"
+        commentData={{
+          userName: '유노',
+          text: text,
+        }}
+      />
     </Container>
   );
 };
