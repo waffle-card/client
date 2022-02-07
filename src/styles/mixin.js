ee90c07bd72815a56495a14b044383e7b49e2386
yoon <yunyun40550082@gmail.com>
2022-02-07 20:34:19 +0900

    refactor: [WC-38] Tab컴포넌트 리팩토링 및 상수, mixin 생성 후 적용

---
+++ src/styles/mixin.js
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+import Common from '@styles';
+
+export const tebItemSize = css`
+  width: calc(100% / 3);
+  height: 47px;
+  min-height: 25px;
+  @media ${Common.media.sm} {
+    height: ${47 * 0.68}px;
+  }
+`;
