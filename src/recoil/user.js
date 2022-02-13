c3652d5d90719fc4e50bd16ffb9de03a22a15419
younoah <kr_uno_10@naver.com>
2022-02-13 22:39:14 +0900

    refactor: 사용하지 않는 코드제거

--- src/recoil/user.js
+++ src/recoil/user.js
@@ -6,7 +6,7 @@ export const userState = atom({
   key: 'user',
   default: selector({
     key: 'user/get',
-    get: async ({ get }) => {
+    get: async () => {
       const token = sessionStorage.getItem(TOKEN_NAME);
       if (!token) return null;
 
@@ -23,28 +23,3 @@ export const userState = atom({
     },
   }),
 });
-
-// export const userSelector = selector({
-//   key: 'user/get',
-//   get: async ({ get }) => {
-//     const userInfo = get(userState);
-//     if (userInfo) return userInfo;
-
-//     const token = sessionStorage.getItem(TOKEN_NAME);
-//     if (!token) return null;
-
-//     try {
-//       const {
-//         data: { id, email, name },
-//       } = await authApi.me(token);
-
-//       return { id, email, name };
-//     } catch (error) {
-//       sessionStorage.removeItem(TOKEN_NAME);
-//       console.error(`in User Recoil: ${error.message}`);
-//     }
-//   },
-//   set: ({ set }, newValue) => {
-//     set(userState, newValue);
-//   },
-// });
