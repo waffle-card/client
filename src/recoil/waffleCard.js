9a792dbe82067b2041067d08bff513105b071b71
younoah <kr_uno_10@naver.com>
2022-02-15 14:35:00 +0900

    docs: TODO 주석 추가

--- src/recoil/waffleCard.js
+++ src/recoil/waffleCard.js
@@ -2,6 +2,8 @@ import { selectorFamily } from 'recoil';
 import { waffleCardApi } from '@apis';
 import { userState } from './user';
 
+// TODO(윤호): 와플카드 전역 상태 Recoil로 변경시도, 하지만 캐시기능 떄문에 리프레쉬의 어려움이 있어서 Context로 대체
+
 const waffleCardsCommand = {
   total: () => {
     return waffleCardApi.getWaffleCards();
