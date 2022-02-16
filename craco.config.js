0e8cacfbd655295d522d260337549d5b12dfc3c8
younoah <kr_uno_10@naver.com>
2022-02-16 18:15:47 +0900

    feat: 타입스크립트로 전환을 위한 패키지 설치 및 설정

--- craco.config.js
+++ craco.config.js
@@ -1,22 +1,17 @@
-const path = require('path');
+const CracoAlias = require('craco-alias');
 
 module.exports = {
   babel: {
     presets: ['@emotion/babel-preset-css-prop'],
   },
-  webpack: {
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@contexts': path.resolve(__dirname, 'src/contexts'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
-      '@validators': path.resolve(__dirname, 'src/validators'),
-      '@apis': path.resolve(__dirname, 'src/apis'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@constants': path.resolve(__dirname, 'src/constants'),
-      '@images': path.resolve(__dirname, 'src/images'),
-      '@recoil': path.resolve(__dirname, 'src/recoil'),
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: './src',
+        tsConfigPath: './tsconfig.path.json',
+      },
     },
-  },
+  ],
 };
