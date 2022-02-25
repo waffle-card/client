1c3a9a4fed9c1204e9cad91a22faa9b300bef735
yoon <yunyun40550082@gmail.com>
2022-02-25 16:52:13 +0900

    chore: [WC-55] 주석제거

--- src/hooks/useClickAway.tsx
+++ src/hooks/useClickAway.tsx
@@ -4,7 +4,6 @@ import { useEffect, useRef } from 'react';
 const events = ['mousedown', 'touchstart'];
 
 const useClickAway = <T extends HTMLElement>(
-  // HTMLElement 제외한 나머지 타입만 넘겨주면 알아서 HTMLElement 도 확장해준다
   handler: (e?: Event) => void,
 ): RefObject<T> => {
   const ref = useRef<T>(null);
@@ -19,8 +18,6 @@ const useClickAway = <T extends HTMLElement>(
     if (!element) return;
 
     const handleEvent: EventListener = e => {
-      // addEventListener에 파라미터로 넘겨주려면 EventListener타입이어야한다
-      // 그러나 여기엔 e.target에 대한 타입이 없어서 as로 설정해줘야한다
       e.stopPropagation();
       !element.contains(e.target as HTMLElement) && savedHandler.current(e);
     };
